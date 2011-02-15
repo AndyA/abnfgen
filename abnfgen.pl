644b16aba999912c63e54f396cace602b22a2566
Andy Armstrong <andy@hexten.net>
2011-02-15 01:58:36 +0000

    WIP.

--- abnfgen.pl
+++ abnfgen.pl
@@ -13,34 +13,80 @@ use constant MAX_DEPTH    => 10;
 my %Rule = ();
 
 sub def(@);
+sub ci($);
 
-def lc_letter => opt( 'a' .. 'z' );
-def uc_letter => opt( 'A' .. 'Z' );
-def letter    => opt( \'lc_letter', \'uc_letter' );
-def var_char  => opt( [ 26, \'letter' ], [ 10, \'digit' ] );
-def var_name => \'letter', rep( \'var_char', 0, 3 );
-def digit => opt( '0' .. '9' );
-def one_to_nine => opt( '1' .. '9' );
-def number => opt( '0', seq( \'one_to_nine', rep( \'digit', 0, 9 ) ) );
-def assign => opt( 'LET ', '' ), \'var_name', ' = ', \'expr';
-def
- for_stmt => 'FOR ',
- \'var_name', ' = ', \'expr', ' TO ', \'expr';
-def next_stmt => 'NEXT';
-def for_next => \'for_stmt', "\n", \'program', \'next_stmt';
-def atom => opt( \'number', \'var_name' );
-def oper => \'expr', ' ', opt( '+', '-', '*', '/' ), ' ', \'expr';
-def paren => '(', \'expr', ')';
-def expr => opt( [ 5, \'atom' ], \'paren', \'oper' );
-def
- print_stmt => 'PRINT ',
- \'expr', rep( seq( opt( ', ', '; ' ), \'expr' ), 0, 3 );
-def statement =>
- opt( [ 5, \'assign' ], [ 5, \'print_stmt' ], \'for_next' );
-def line => \'statement', "\n";
-def program => rep( \'line', 0, 8 );
+if ( 0 ) {
+  def lc_letter => opt( 'a' .. 'z' );
+  def uc_letter => opt( 'A' .. 'Z' );
+  def letter    => opt( \'lc_letter', \'uc_letter' );
+  def word      => rep( \'lc_letter', 1, 20, 3 );
+  def
+   sentence => seq( \'uc_letter', \'word' ),
+   rep( seq( ' ', \'word' ), 0, 10, 3 ), '.';
+  def var_char => opt( [ 26, \'letter' ], [ 10, \'digit' ] );
+  def var_name => \'letter', rep( \'var_char', 0, 10, 3 );
+  def digit => opt( '0' .. '9' );
+  def one_to_nine => opt( '1' .. '9' );
+  def number =>
+   opt( '0', seq( \'one_to_nine', rep( \'digit', 0, 9, 3 ) ) );
+  def assign => opt( 'LET ', '' ), \'var_name', ' = ', \'num_expr';
+  def
+   for_stmt => 'FOR ',
+   \'var_name', ' = ', \'num_expr', ' TO ', \'num_expr';
+  def next_stmt => 'NEXT';
+  def for_next => \'for_stmt', "\n", \'program', \'next_stmt';
+  def atom => opt( \'number', \'var_name' );
+  def
+   oper => \'num_expr',
+   ' ', opt( '+', '-', '*', '/' ), ' ', \'num_expr';
+  def paren => '(', \'num_expr', ')';
+  def num_expr => opt( [ 5, \'atom' ], \'paren', \'oper' );
+  def
+   print_stmt => 'PRINT ',
+   \'num_expr', rep( seq( opt( ', ', '; ' ), \'num_expr' ), 0, 3 );
+  def statement =>
+   opt( [ 5, \'assign' ], [ 5, \'print_stmt' ], \'for_next' );
+  def line => \'statement', "\n";
+  def program => rep( \'line', 0, 8 );
 
-print rule( 'program' )->();
+  print rule( 'program' )->();
+  print rule( 'sentence' )->(), "\n";
+}
+
+# TODO Fully evaluate all args - so that, e.g. the parameters to rep can
+# be runtime variables.
+
+# TODO Implement generators
+
+if ( 1 ) {
+  def lc_letter => opt( 'a' .. 'z' );
+  def uc_letter => opt( 'A' .. 'Z' );
+  def letter    => opt( \'lc_letter', \'uc_letter' );
+  def word      => rep( \'lc_letter', 1, 20, 3 );
+
+  def body => rep( \'xml', 0, 10, 2 );
+  def attr => ' ', \'word', '="', \'word', '"';
+  def attrs => rep( \'attr', 0, 5, 2 );
+
+  def xml => opt(
+    seq( '<', \'word', \'attrs', '/>' ),
+    context(
+      {
+        tag => sub { const( \'word' ) }
+      },
+      '<',
+      \'tag',
+      \'attrs',
+      '>',
+      \'body',
+      '</',
+      \'tag',
+      '>'
+    )
+  );
+
+  print rule( 'xml' )->(), "\n";
+}
 
 # Accepts a list of options of this form:
 #
@@ -59,6 +105,38 @@ sub def(@) {
   $Rule{$name} = seq( @code );
 }
 
+sub ci($) {
+  my @part = split /\b/, shift;
+  my @seq = ();
+  for my $p ( @part ) {
+    if ( $p =~ /\w/ ) {
+      push @seq, map { opt( lc $_, uc $_ ) } split //, $p;
+    }
+    else {
+      push @seq, opt( $p );
+    }
+  }
+  return seq( @seq );
+}
+
+sub const {
+  my $v = seq( @_ )->();
+  return sub { $v };
+}
+
+sub context {
+  my ( $ctx, @seq ) = @_;
+  my %bind  = %$ctx;
+  my %stash = ();
+  return sub {
+    local @Rule{ keys %bind };
+    while ( my ( $k, $v ) = each %bind ) {
+      $Rule{$k} = opt( $v->() );
+    }
+    return seq( @seq )->();
+   }
+}
+
 sub limit {
   my $cb = shift;
   my $limiter ||= do {
@@ -68,8 +146,8 @@ sub limit {
       ++$depth;
       if ( $depth >= MAX_DEPTH ) {
         --$depth;
-        warn "DEPTH LIMIT\n";
-        return '';
+        #        warn "DEPTH LIMIT\n";
+        return ' *** ';
       }
       my $rv = $cb->();
       --$depth;
@@ -125,11 +203,18 @@ sub seq {
   };
 }
 
+sub pow_rand {
+  my ( $limit, $power ) = @_;
+  rand( $limit**( 1 / $power ) )**$power;
+}
+
 sub rep {
   my $cb = opt( shift );
-  my ( $min, $max ) = @_;
+  my ( $min, $max, $pow ) = @_;
+  $pow = 1 unless defined $pow;
   return sub {
-    join '', map { $cb->() } 1 .. $min + rand( $max + 1 - $min );
+    join '',
+     map { $cb->() } 1 .. $min + pow_rand( $max + 1 - $min, $pow );
   };
 }
 
@@ -141,4 +226,3 @@ sub rule {
 }
 
 # vim:ts=2:sw=2:sts=2:et:ft=perl
-
